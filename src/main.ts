5121f0da27b1383dae8aecb3dbe350f4f5fb85e3
Nazmus Sakib <sakib.tnt@gmail.com>
2022-03-19 12:00:29 +0600

    doc

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const swaggerCconfig = new DocumentBuilder()
     .setTitle('Courier Api')
